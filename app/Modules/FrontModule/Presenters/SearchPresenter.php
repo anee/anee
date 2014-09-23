9d212c3da28f4914bf45c38c0ef703b5b8cbe234
Lukáš Drahník <L.Drahnik@gmail.com>
2014-09-24 00:22:22 +0200

    Searching as service

--- app/Modules/FrontModule/Presenters/SearchPresenter.php
+++ app/Modules/FrontModule/Presenters/SearchPresenter.php
@@ -2,9 +2,8 @@
 
 namespace App\Modules\FrontModule\Presenters;
 
-use App\Modules\FrontModule\Forms;
-use App\FilterUtils;
-
+use App\Modules\FrontModule\Components;
+use App\Searching;
 
 
 /**
@@ -12,51 +11,17 @@ use App\FilterUtils;
  */
 class SearchPresenter extends BasePresenter
 {
-    private $values;
+	/** @var \App\Searching\SearchFactory @inject*/
+	public $searching;
 
     public function actionDefault()
     {
-        $this->values = FilterUtils::checkArray($this->getParameter('values'));
+		$this->searching->setValues($this->getParameter('values'));
     }
 
 	public function renderDefault()
 	{
-        $this->template->values = $this->values;
-
-        $events = $this->eventRepository->findByFilters($this->values);
-        $tracks = $this->trackRepository->findByFilters($this->values);
-        $places = $this->placeRepository->findByFilters($this->values);
-        $photos = $this->photoRepository->findByFilters($this->values);
-
-		$eventsCount = $this->eventRepository->findByFiltersCount($this->values);
-        $tracksCount = $this->trackRepository->findByFiltersCount($this->values);
-        $placesCount = $this->placeRepository->findByFiltersCount($this->values);
-        $photosCount = $this->photoRepository->findByFiltersCount($this->values);
-
-        $entityObject = null;
-        $entityUrl = "";
-        if($this->values['filterEntity'] == 'Event') {
-            $entityObject = $this->eventRepository->findById($this->values['filterEntityId']);
-            $entityUrl = 'Event:detail';
-        } elseif ($this->values['filterEntity'] == 'Track') {
-            $entityObject = $this->trackRepository->findById($this->values['filterEntityId']);
-            $entityUrl = 'Track:detail';
-        } elseif ($this->values['filterEntity'] == 'Place') {
-            $entityObject = $this->placeRepository->findById($this->values['filterEntityId']);
-        }
-
-        $this->template->results = array(
-            'EntityObject' => $entityObject,
-            'EntityUrl' => $entityUrl,
-            'Count' => count($events) + count($tracks) + count($places) + count($photos),
-            'Events' => $events,
-            'Tracks' => $tracks,
-            'Places' => $places,
-            'Photos' => $photos,
-            'EventsCount' => $eventsCount,
-            'TracksCount' => $tracksCount,
-            'PlacesCount' => $placesCount,
-            'PhotosCount' => $photosCount
-        );
+		$this->template->values = $this->searching->getValues();
+		$this->template->results = $this->searching->getNewResults();
 	}
 }
---
+++ app/libs/Searching/SearchFactory.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Searching;
+
+use App\Model;
+use Nette;
+
+
+
+/**
+ * Author Lukáš Drahník <L.Drahnik@gmail.com>
+ */
+class SearchFactory extends Nette\Object
+{
+	/** @var \App\Model\EventRepository */
+    private $eventRepository;
+
+	/** @var \App\Model\TrackRepository */
+	private $trackRepository;
+
+	/** @var \App\Model\PhotoRepository */
+	private $photoRepository;
+
+	/** @var \App\Model\PlaceRepository */
+	private $placeRepository;
+
+	/** @var array */
+	private $values = array();
+
+	/** @var array */
+	private $results = array();
+
+    public function __construct(Model\EventRepository $eventRepository, Model\TrackRepository $trackRepository, Model\PhotoRepository $photoRepository, Model\PlaceRepository $placeRepository)
+    {
+		$this->eventRepository = $eventRepository;
+		$this->trackRepository  = $trackRepository;
+		$this->photoRepository  = $photoRepository;
+		$this->placeRepository  = $placeRepository;
+
+		$this->values = Utils::clearValuesArray();
+		$this->results = Utils::clearResultsArray();
+    }
+
+	public function getNewResults() {
+		$events = $this->eventRepository->findByFilters($this->values);
+		$tracks = $this->trackRepository->findByFilters($this->values);
+		$places = $this->placeRepository->findByFilters($this->values);
+		$photos = $this->photoRepository->findByFilters($this->values);
+
+		$eventsCount = $this->eventRepository->findByFiltersCount($this->values);
+		$tracksCount = $this->trackRepository->findByFiltersCount($this->values);
+		$placesCount = $this->placeRepository->findByFiltersCount($this->values);
+		$photosCount = $this->photoRepository->findByFiltersCount($this->values);
+
+		$entityObject = null;
+		$entityUrl = "";
+		if ($this->values['filterEntity'] == 'Event') {
+			$entityObject = $this->eventRepository->findById($this->values['filterEntityId']);
+			$entityUrl = 'Event:detail';
+		} elseif ($this->values['filterEntity'] == 'Track') {
+			$entityObject = $this->trackRepository->findById($this->values['filterEntityId']);
+			$entityUrl = 'Track:detail';
+		} elseif ($this->values['filterEntity'] == 'Place') {
+			$entityObject = $this->placeRepository->findById($this->values['filterEntityId']);
+		}
+
+		$this->results = array(
+			'EntityObject' => $entityObject,
+			'EntityUrl' => $entityUrl,
+			'Count' => count($events) + count($tracks) + count($places) + count($photos),
+			'Events' => $events,
+			'Tracks' => $tracks,
+			'Places' => $places,
+			'Photos' => $photos,
+			'EventsCount' => $eventsCount,
+			'TracksCount' => $tracksCount,
+			'PlacesCount' => $placesCount,
+			'PhotosCount' => $photosCount
+		);
+		return $this->results;
+	}
+
+	public function setValues($values)
+	{
+		$this->values = Utils::checkValuesArray($values);
+	}
+
+	public function getResults()
+	{
+		return $this->results;
+	}
+
+	public function getValues()
+	{
+		return $this->values;
+	}
+}
