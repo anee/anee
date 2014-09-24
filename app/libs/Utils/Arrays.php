fa115e8b3e1c954f31d134df01a61a68429d717d
Lukáš Drahník <L.Drahnik@gmail.com>
2014-09-24 13:18:14 +0200

    Renamed array to arr

---
+++ app/libs/Utils/Arrays.php
@@ -0,0 +1,66 @@
+<?php
+
+
+namespace App\Utils;
+
+
+
+/**
+ * Author Lukáš Drahník <L.Drahnik@gmail.com>
+ */
+class Arrays {
+
+
+    public static function timeSubFilterTime($filterTime)
+    {
+        $time = new \DateTime;
+        if($filterTime == 'Past hour') {
+            return $time->sub(new \DateInterval('PT1H'));
+        } else if ($filterTime == 'Past week') {
+            return $time->sub(new \DateInterval('P7D'));
+        } else if ($filterTime == 'Past month') {
+            return $time->sub(new \DateInterval('P30D'));
+        } else if ($filterTime == 'Past year') {
+            return $time->sub(new \DateInterval('P365D'));
+        } else {
+            return $time;
+        }
+    }
+
+    public static function arrayContainsOrEmpty($value, $arr)
+    {
+        if(empty($arr) == true) {
+            return true;
+        } else {
+            foreach($arr as $i => $v) {
+                if($v == $value) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    public static function arrayContains($value, $arr)
+    {
+        foreach($arr as $i => $v) {
+            if($v == $value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+	public static function arrayContainsOnly($value, $arr)
+	{
+		if(count($arr) != 1) {
+			return false;
+		}
+		foreach($arr as $i => $v) {
+			if($v == $value) {
+				return true;
+			}
+		}
+		return false;
+	}
+} 
