10f48820222ca1effe4d9fa049bf9cd738457023
Lukáš Drahník <L.Drahnik@gmail.com>
2014-09-24 16:07:03 +0200

    Fixed

--- app/Modules/FrontModule/Presenters/HomepagePresenter.php
+++ app/Modules/FrontModule/Presenters/HomepagePresenter.php
@@ -13,7 +13,7 @@ class HomepagePresenter extends BasePresenter
 	public function renderDefault()
 	{
         $this->template->event = $this->eventBaseLogic->findLast();
-        $this->template->tracks = $this->trackRepository->findLastByCount(2);
-        $this->template->photos = $this->photoRepository->findLastByCount(4);
+        $this->template->tracks = $this->trackBaseLogic->findLastByCount(2);
+        $this->template->photos = $this->photoBaseLogic->findLastByCount(4);
 	}
 }
