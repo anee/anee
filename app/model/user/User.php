6fa5312a165db381277725c022a048b02f4af604
Lukáš Drahník <L.Drahnik@gmail.com>
2014-10-11 19:54:55 +0200

    Added asociations and public variable

--- app/model/user/User.php
+++ app/model/user/User.php
@@ -17,6 +17,11 @@ class User extends BaseEntity {
 	protected $username;
 
 	/**
+	 * @ORM\Column(type="boolean")
+	 */
+	protected $public;
+
+	/**
 	 * @ORM\Column(type="string", length=128, unique=true)
 	 */
 	protected $email;
@@ -27,14 +32,36 @@ class User extends BaseEntity {
 	protected $password;
 
 	/**
+	 * @ORM\OneToMany(targetEntity="App\Model\Track", mappedBy="user")
+	 */
+	protected $tracks;
+
+	/**
+	 * @ORM\OneToMany(targetEntity="App\Model\Place", mappedBy="user")
+	 */
+	protected $places;
+
+	/**
+	 * @ORM\OneToMany(targetEntity="App\Model\Transport", mappedBy="user")
+	 */
+	protected $transports;
+
+	/**
+	 * @ORM\OneToMany(targetEntity="App\Model\Photo", mappedBy="user")
+	 */
+	protected $photos;
+
+	/**
 	 * @param $username
+	 * @param $public
 	 * @param $email
 	 * @param $password
 	 */
-	public function __construct($username, $email, $password)
+	public function __construct($username, $public, $email, $password)
 	{
 		$this->username = $username;
 		$this->email = $email;
+		$this->public = $public;
 		$this->password = Passwords::hash($password);
 	}
 }
