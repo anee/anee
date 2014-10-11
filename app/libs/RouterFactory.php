223d6cc71af600d8dc7baf5bef6f54b1d08a325f
Lukáš Drahník <L.Drahnik@gmail.com>
2014-10-11 19:54:01 +0200

    Refactored routes

--- app/libs/RouterFactory.php
+++ app/libs/RouterFactory.php
@@ -21,51 +21,33 @@ class RouterFactory
 	{
 		$router = new RouteList();
 
-
-		// FRONTEND MODULE
-		$router[] = new Route('<action>', array(
-			'module' => 'Frontend',
-			'presenter' => 'Homepage',
+		//FRONTEND
+		$router[] = new Route('<username>', array(
+			'module' => 'backend',
+			'presenter' => 'Profile',
+			'action' => 'default',
 		));
 
 		// SECURITY MODULE
-		$router[] = new Route('<presenter>/<action>', array(
-			'module' => 'Security',
-		));
+		$security = new RouteList('Security');
+		$security[] = new Route('<presenter>/<action>');
+		$router[] = $security;
 
 		// BACKEND MODULE
 		$backend = new RouteList('Backend');
-		$backend[] = new Route('<presenter>/<action>/<id>', array(
+		$backend[] = new Route('<username>/<presenter>/<id>', array(
+			'presenter' => 'Homepage',
+			'id' => NULL,
+			'username' => NULL,
+			'action' => 'default',
+		));
+		$backend[] = new Route('<username>/<presenter>/<action>/<id>', array(
 			'presenter' => 'Homepage',
 			'action' => 'default',
 			'id' => NULL,
+			'username' => NULL,
 		));
 		$router[] = $backend;
-
-
-		/*$backend = new RouteList('Backend');
-		$backend[] = new Route('<username>/<presenter>/<action>/<id>', array(
-			'username' => NULL,
-			'presenter' => 'Homepage',
-			'action' => 'default',
-			'id' => NULL,
-		));
-		$router[] = $backend;*/
-
-		/*$backend = new RouteList('Backend');
-		$backend[] = new Route('<username>/<presenter>/<action>/<id>', array(
-			'username' => NULL,
-			'presenter' => 'Homepage',
-			'action' => 'default',
-			'id' => NULL,
-		));
-		$router[] = $backend;
-
-		$router[] = new Route('<module>/<presenter>/<action>', array(
-			'module' => 'Frontend',
-			'presenter' => 'Homepage',
-			'action' => 'default',
-		));*/
 
 		return $router;
 	}
