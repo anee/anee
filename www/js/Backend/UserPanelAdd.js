283af8cb30ff89361c390d347bf19f7a9dbdf286
Lukáš Drahník <L.Drahnik@gmail.com>
2014-10-25 22:10:54 +0200

    Refactored structure

---
+++ www/js/Backend/UserPanelAdd.js
@@ -0,0 +1,39 @@
+/**
+ * Author Lukáš Drahník <L.Drahnik@gmail.com>
+ */
+
+/**
+ * Hide/Show user-panel-add modal
+ */
+/*function userPanelAdd() {
+    var e = document.getElementById('user-panel-add');
+    if(e.style.display == 'none') {
+        e.style.display = 'block';
+    } else {
+        e.style.display = 'none';
+    }
+}*/
+
+// aktivace odkazu na zobrazeni dialogu
+jQuery(function($) {
+    $('a.ajaxdialog').live('click', function(event) {
+        event.preventDefault();
+        $.post($.nette.href = this.href, function(data) {
+            // (mimo jine) injektovani formulare do HTML
+            $.nette.success(data);
+            // aktivace ajaxoveho submitu formulare
+            activateAjaxForm();
+            // zobrazeni formulare v dialogu
+            $("#snippet--simpleForm").dialog();
+        }, "json");
+    });
+});
+// aktivace "ajaxoveho" formulare
+function activateAjaxForm () {
+    $("#snippet--simpleForm form :submit").click(function () {
+        $(this).ajaxSubmit();
+        $("#snippet--simpleForm").dialog( "destroy" ); // po submitnuti zavreme dialog
+        return false;
+    });
+}
+
