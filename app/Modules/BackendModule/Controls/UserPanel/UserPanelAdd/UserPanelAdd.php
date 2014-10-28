ade3d7981ee214aa73f7ef4b885a51f476a2968d
Lukáš Drahník <L.Drahnik@gmail.com>
2014-10-29 00:06:53 +0100

    Attemp on modal

--- app/Modules/BackendModule/Controls/UserPanel/UserPanelAdd/UserPanelAdd.php
+++ app/Modules/BackendModule/Controls/UserPanel/UserPanelAdd/UserPanelAdd.php
@@ -5,6 +5,7 @@ namespace App\Modules\BackendModule\Controls;
 use App\Model\UserBaseLogic;
 use Nette;
 use Nette\Application\UI\Control;
+use Nette\Forms\Form;
 
 
 /**
@@ -26,4 +27,36 @@ class UserPanelAdd extends Control
 		$this->template->setFile($file);
 		$this->template->render();
 	}
+
+	/*public function handleAdd()
+	{
+		$this->template->showSimpleForm = true;
+		if ($this->presenter->isAjax()) {
+			$this->invalidateControl('simpleForm');
+		}
+	}*/
+
+	/*protected function createComponentSimpleForm()
+	{
+		$form = new Nette\Application\UI\Form;
+		$form->addText('email', 'Email:')
+			->addRule(Form::FILLED, 'Zadejte email');
+
+		$form->addSubmit('register', 'Registrovat')
+			->onClick[] = callback($this, 'simpleFormSubmitted');
+
+		return $form;
+	}
+
+	public function simpleFormSubmitted($btn)
+	{
+		// ... zde se zpracuj vysledky z $btn->form->getValues()
+		$this->flashMessage('Vysledek byl ulozen/zpracovan.');
+		if ($this->presenter->isAjax()) {
+			$this->invalidateControl('simpleForm');
+			$this->invalidateControl('flashMsg');
+		} else {
+			$this->redirect('this');
+		}
+	}*/
 }
