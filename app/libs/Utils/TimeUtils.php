d0db734b322a0c69798b25ac153c917e5a4178c8
Lukáš Drahník <L.Drahnik@gmail.com>
2014-11-25 03:41:09 +0100

    [Added] util for convert span to seconds

---
+++ app/libs/Utils/TimeUtils.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace App\Utils;
+
+
+
+/**
+ * Author Lukáš Drahník <L.Drahnik@gmail.com>
+ */
+class TimeUtils {
+
+	public static function fromSecondsHours($secParam) {
+		$hours = floor($secParam / 3600);
+		return $hours;
+	}
+	public static function fromSecondsMinutes($secParam) {
+		$minutes = floor(($secParam / 60) % 60);
+		return $minutes;
+	}
+	public static function fromSecondsSeconds($secParam) {
+		$seconds = $secParam % 60;
+		return $seconds;
+	}
+
+	public static function fromSpanToSeconds($hours, $minutes, $seconds) {
+		$secondsTotal = 0;
+		if($hours != "") {
+			$secondsTotal += $hours * 3600;
+		}
+		if($minutes != "") {
+			$secondsTotal += $minutes * 60;
+		}
+		if($hours != "") {
+			$secondsTotal += $seconds;
+		}
+		return $secondsTotal;
+	}
+} 
