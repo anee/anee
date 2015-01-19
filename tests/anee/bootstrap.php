7ce0f9843e8dbc55be690539b32216f4992f12b9
Lukáš Drahník <L.Drahnik@gmail.com>
2015-01-19 16:45:17 +0100

    [Added] Init tests

--- tests/anee/bootstrap.php
+++ tests/anee/bootstrap.php
@@ -1,6 +1,8 @@
 <?php
-
-require __DIR__ . '/../vendor/autoload.php';
+/**
+ * Author Lukáš Drahník <L.Drahnik@gmail.com>
+ */
+require __DIR__ . '/../../vendor/autoload.php';
 
 if (!class_exists('Tester\Assert')) {
 	echo "Install Nette Tester using `composer update --dev`\n";
@@ -11,11 +13,14 @@ Tester\Environment::setup();
 
 $configurator = new Nette\Configurator;
 $configurator->setDebugMode(FALSE);
-$configurator->setTempDirectory(__DIR__ . '/../temp');
+$configurator->setTempDirectory(__DIR__ . '../../temp');
 $configurator->createRobotLoader()
-	->addDirectory(__DIR__ . '/../app')
+	->addDirectory(__DIR__ . '/../../app')
 	->register();
 
-$configurator->addConfig(__DIR__ . '/../app/config/config.neon');
-$configurator->addConfig(__DIR__ . '/../app/config/config.local.neon');
+
+// unused
+$configurator->addConfig(__DIR__ . '/config/config.neon');
+
 return $configurator->createContainer();
+
