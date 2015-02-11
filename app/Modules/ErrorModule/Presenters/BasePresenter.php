a5209be018b55b74c1f150706a3de520cd1a578c
Lukáš Drahník <L.Drahnik@gmail.com>
2015-02-11 23:47:16 +0100

    Every error handle 500

--- app/Modules/ErrorModule/Presenters/BasePresenter.php
+++ app/Modules/ErrorModule/Presenters/BasePresenter.php
@@ -10,6 +10,20 @@ use Nette;
  */
 abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
+	/**
+	 * @var \ViewKeeper\ViewKeeper
+	 * @inject
+	 */
+	public $keeper;
 
+	public function formatLayoutTemplateFiles()
+	{
+		return array($this->keeper->getView('Error:Base', 'layouts', 'layout'));
+	}
+
+	public function formatTemplateFiles()
+	{
+		return array($this->keeper->getView($this->name, 'presenters', '500')); // every time it ll be 500
+	}
 }
 
--- app/Modules/ErrorModule/Presenters/ErrorPresenter.php
+++ app/Modules/ErrorModule/Presenters/ErrorPresenter.php
@@ -21,12 +21,15 @@ class ErrorPresenter extends BasePresenter
 		if ($exception instanceof Nette\Application\BadRequestException) {
 			$code = $exception->getCode();
 			// load template 403.latte or 404.latte or ... 4xx.latte
-			$this->setView(in_array($code, array(403, 404, 405, 410, 500)) ? $code : '4xx');
+			//Debugger::log($this->keeper->getView($this->name, 'presenters', (in_array($code, array(403, 404, 405, 410, 500)) ? $code : '4xx'), NULL));
+			//$this->setView($this->keeper->getView($this->name, 'presenters', (in_array($code, array(403, 404, 405, 410, 500)) ? $code : '4xx'), NULL));
+			$this->setView($this->getView() . in_array($code, array(403, 404, 405, 410, 500)) ? $code : '4xx');
 			// log to access.log
 			Debugger::log("HTTP code $code: {$exception->getMessage()} in {$exception->getFile()}:{$exception->getLine()}", 'access');
 
 		} else {
-			$this->setView('500'); // load template 500.latte
+			$this->setView($this->getView() . '500');
+			//$this->setView($this->keeper->getView($this->name, 'presenters', '500', NULL));
 			Debugger::log($exception, Debugger::ERROR); // and log exception
 		}
 
