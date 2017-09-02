723939e1508156f886ce5058366124c547a06769
Lukáš Drahník <ldrahnik@gmail.com>
2017-09-02 13:54:26 +0200

    init.sh: updated

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
 #usr/bin/sh
 
-mkdir -p ./temp/cache
-mkdir -p ./www/webtemp
-cp ./app/config/config.local.default.neon ./app/config/config.local.neon
 composer update
+chmod a+w -R log temp www
+cp ./app/config/config.local.default.neon ./app/config/config.local.neon
